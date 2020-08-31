23cdbe3dc5ce4ca6441f9a551a1dc1fe281dd92c
kang_jisu <vkfkd1107@github.com>
2020-09-01 00:21:30 +0900

    param chat,name to home.html

--- firstproject/firstapp/views.py
+++ firstproject/firstapp/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request,'home.html')
+    chat='hello'
+    name='jisu'
+
+    return render(request,'home.html',{'user_chat':chat,'user_name':name})
