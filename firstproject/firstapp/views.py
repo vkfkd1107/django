dc38b623e2a7a6aa61547ea4345964bc4e4746af
kang_jisu <vkfkd1107@github.com>
2020-09-01 00:38:44 +0900

    add result.html, form

--- firstproject/firstapp/views.py
+++ firstproject/firstapp/views.py
@@ -2,8 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+students=['jisu','minso','suho']
+
 def home(request):
     chat='hello'
     name='jisu'
 
     return render(request,'home.html',{'user_chat':chat,'user_name':name})
+
+def result(request):
+    name=request.POST['username']
+
+    if name in students:
+        is_exist=True
+    else:
+        is_exist=False
+    
+    return render(request,'result.html',{'user_name':name,'is_exist':is_exist})
