342845f14c9e59a336d2eb9182fbcbb50a68007d
kang_jisu <vkfkd1107@github.com>
2020-09-01 00:54:24 +0900

    add models

--- firstproject/firstapp/admin.py
+++ firstproject/firstapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import AiClass, AiStudents
 
 # Register your models here.
+
+admin.site.register(AiClass)
+admin.site.register(AiStudents)
--- firstproject/firstapp/models.py
+++ firstproject/firstapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class AiClass(models.Model):
+    class_num=models.IntegerField()
+    lecturer=models.CharField(max_length=30)
+    class_room=models.CharField(max_length=30)
+    students_num=models.IntegerField()
+
+class AiStudents(models.Model):
+    name=models.CharField(max_length=30)
+    phone_num=models.CharField(max_length=30)
+    intro_text=models.TextField()
+    intro_text=models.TextField()
